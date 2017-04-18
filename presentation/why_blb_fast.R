3d0a2fadddcb48d2c447b08d2a645700f5778229
Xingche <xguo@iastate.edu>
2017-04-18 17:14:07 -0500

    Add files via upload

--- presentation/why_blb_fast.R
+++ presentation/why_blb_fast.R
@@ -1,5 +1,5 @@
 n <- 100000
-b <- 500
+b <- 100
 N <- rnorm(n)
 B <- sample(N,b)
 
@@ -10,7 +10,7 @@ return(mean(X))
 
 mean2 <- function(B,n,b){
   X <- as.numeric( rmultinom(1,n,rep(1/b,b)) )
-  return(mean(X*B)/b)
+  return(sum(X*B)/n)
 }
 
 mean1(B,n)
