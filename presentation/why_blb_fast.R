f53df6af201f0ce2a4ac4d13852009aa981978ed
Xingche <xguo@iastate.edu>
2017-04-18 22:08:02 -0500

    Add files via upload

--- presentation/why_blb_fast.R
+++ presentation/why_blb_fast.R
@@ -3,6 +3,11 @@ b <- 100
 N <- rnorm(n)
 B <- sample(N,b)
 
+mean0 <- function(N,n){
+  X <- sample(N, n, replace = TRUE)
+  return(mean(X)) 
+}
+
 mean1 <- function(B,n){
 X <- sample(B, n, replace = TRUE)
 return(mean(X))
@@ -13,7 +18,8 @@ mean2 <- function(B,n,b){
   return(sum(X*B)/n)
 }
 
+mean0(N,n)
 mean1(B,n)
 mean2(B,n,b)
-microbenchmark::microbenchmark(mean1(B,n), mean2(B,n,b))
+microbenchmark::microbenchmark(mean0(N,n),mean1(B,n), mean2(B,n,b))
 
